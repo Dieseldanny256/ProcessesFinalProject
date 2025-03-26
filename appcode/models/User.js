64a50e056ebe3bee534e3bcaa7de1e077428c854
Christopher Ferreira <fartops34@gmail.com>
2025-03-25 23:12:18 -0400

    Update User.js
    Update to the working code

--- appcode/models/User.js
+++ appcode/models/User.js
@@ -1,27 +1,29 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userId : {type: Number, required: true},
+  userId: { type: Number, required: true, unique: true },
   login: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  displayName: { type: String, required: true },
+  email: { type: String, required: true},
   isVerified: { type: Boolean, default: false },
   verificationCode: { type: String },
-  friends: {type: Array},
-  friendRequests: {type: Array},
-  friendRequestsSent: {type: Array},
-  profile: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'}
-
+  friends: { type: [Number], default: [] },
+  friendRequests: { type: [Number], default: [] },
+  friendRequestsSent: { type: [Number], default: [] },
+  profile: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }
 });
 
 const profileSchema = new mongoose.Schema({
-  userId : {type: Number, required: true},
-  displayName: {type: String, required: true},
-  streak: {type: Number, default: 0},
-  powerlevel: {type: Number, default: 0},
-  stats: {type: Array},
-  profilePicture: {type: Number, default: 0}
-})
+  userId: { type: Number, required: true, unique: true },
+  displayName: { type: String, required: true },
+  streak: { type: Number, default: 0 },
+  powerlevel: { type: Number, default: 0 },
+  stats: { type: [String], default: [] },
+  profilePicture: { type: Number, default: 0 }
+});
 
-module.exports = mongoose.model('User', userSchema);
-module.exports = mongoose.model('Profile', profileSchema);
+const User = mongoose.model('User', userSchema);
+const Profile = mongoose.model('Profile', profileSchema);
+
+module.exports = { User, Profile };
